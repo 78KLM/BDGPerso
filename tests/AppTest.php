7559d543b576bc0b74d7859b7561fc2b1e6d14f2
78KLM <kalembaexoce@gmail.com>
2025-03-17 15:54:02 +0100

    pp

---
+++ tests/AppTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+    public function testTestAreWorking(){
+        $this->assertEquals(2, 1+1);
+    }
+}
